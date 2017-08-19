37013bed0fb50bc4568c903609f83c0dedf64202
akhil-tadanki <30665115+akhil-tadanki@users.noreply.github.com>
2017-08-19 17:03:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article1',function(req,res){
+   res.send('art1 requested'); 
+});
+app.get('/article2',function(req,res){
+   res.send('art2 requested'); 
+});
+app.get('/article3',function(req,res){
+   res.send('art3 requested'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
